f64963554d410b41dde1d793ef930574c45b0a25
GameDev <arcademissile@gmail.com>
2017-08-29 10:19:23 -0500

    Added hello world project

---
+++ HelloWorld/Assets/Scripts/Destroyer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour {
+
+	public float destroyTime;
+	public float lerpSpeed;
+
+	bool destroying = false;
+	Vector3 startingScale;
+	float lerp = 0;
+
+	void Start () {
+		Invoke ("Destroy", destroyTime);	
+	}
+
+	void Destroy() {
+		destroying = true;
+		startingScale = transform.localScale;
+	}
+	
+	void Update () {
+		if (destroying) {
+			lerp = Mathf.Min (1, lerp + lerpSpeed * Time.deltaTime);
+
+			if (lerp > 0.99f) {
+				Destroy (gameObject);
+			}
+			transform.localScale = Vector3.Lerp (startingScale, Vector3.zero, lerp);
+		}
+	}
+}
---
+++ HelloWorld/Assets/Scripts/Spawner.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+	public GameObject[] objects;
+
+	void Update () {
+		int objIndex = Random.Range (0, objects.Length);
+		var obj = objects [objIndex];
+		GameObject.Instantiate (obj, transform);
+	}
+}
